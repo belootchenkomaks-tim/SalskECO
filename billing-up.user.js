531ae430ff696156d37470b0df2b94f5d8e74346
belootchenkomaks-tim <belootchenkomaks@gmail.com>
2026-03-07 06:45:00 +0300

    Update billing-up.user.js

--- billing-up.user.js
+++ billing-up.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         BILLING UP
 // @namespace    http://tampermonkey.net/
-// @version      7.11
+// @version      8.0
 // @description  Собирает данные с billing.timernet.ru и ищет по номеру договора в USERSIDE
 // @author       You
 // @match        https://billing.timernet.ru/*
